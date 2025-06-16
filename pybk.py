186fa59bddfb7b6b2274f3a7e1eed0ccbf7e91d2
Pigeon Nation <134137685+pigeon-nation@users.noreply.github.com>
2025-06-16 12:20:44 +1000

    Version 1.1.0 - Internal re-write. Intended API remains identical.
    Now entirely OOP-based.

--- pybk.py
+++ pybk.py
@@ -1,4 +1,7 @@
 class DynamicReturn:
+    def __init__(self, /, *data):
+        self.data = data
+
     def __call__(self):
         if len(self.data) == 0:
             return None
@@ -8,23 +11,23 @@ class DynamicReturn:
             return self.data
 
 class ReturnVal(DynamicReturn):
-    def __init__(self, /, *data):
-        self.data = data
-    
+    def __repr__(self):
+        return "<ReturnVal> " + repr(self.__call__())
+
 class Hashable(DynamicReturn):
-    def __init__(self, /, *data):
-        self.data = data
+    def __repr__(self):
+        return "<Hashable> " + repr(self.__call__())
 
 class AsmModule:
     def __init__(self, funcs):
         for fname in funcs.keys():
-            setattr(self, fname, retw(funcs[fname]))
+            setattr(self, fname, self._ret_wrapper(funcs[fname]))
 
-def retw(f):
-    def wrapf(*a, **k):
-        setr = f(*a, **k)
-        for item in setr:
-            if isinstance(item, ReturnVal):
-                return item()
-        return None
-    return wrapf
+    def _ret_wrapper(self, f):
+        def _wrapper_func(*a, **k):
+            setr = f(*a, **k)
+            for item in setr:
+                if isinstance(item, ReturnVal):
+                    return item()
+            return None
+        return _wrapper_func
